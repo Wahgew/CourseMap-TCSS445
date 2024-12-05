78fb9938439d6752bfcfe08a7824c2a778576263
Elias Arriola <earrio@uw.edu>
2024-12-05 13:18:46 -0800

    documentation for queries

--- EliasCourseMap/controllers/query1.js
+++ EliasCourseMap/controllers/query1.js
@@ -1,5 +1,5 @@
 //Elias Arriola
-//The following file is the backend of query1.html which queries for professor information.
+//The following file is the backend of query1.html which corresponds to query 1 in the final script.
 // Step 1: Import Required Libraries 
 // Import the database configuration to interact with the MySQL database. 
 const db = require('../dbConfig'); 
